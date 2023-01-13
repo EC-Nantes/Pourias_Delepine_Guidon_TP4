5c049f7c6fafdcdb9a937e9c1da541d4972b0775
RobinDELEPINE <robin.delepine1901@gmail.com>
2023-01-13 11:54:48 +0100

    parcelle presque finie

--- main.cpp
+++ main.cpp
@@ -8,9 +8,10 @@ int main(int argc, char const *argv[])
 {
     std::cout << "hello world"<< std::endl;
     point2D<int> pt(6, 7);
-    int num = 1;
-    float surface = 0.5;
-    //Polygone<int, float> pol(num, surface);
-    //parcelle parc(1, "owner", pol);
+
+    Polygone<int> pol;
+    pol.addPoint(pt);
+
+    parcelle<int> parc(1, "owner", pol);
     return 0;
 }
