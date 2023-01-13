004a7e68083d81aad4ce49c27b21f8887a1a4f85
Xquiratis <rom.pourias@gmail.com>
2023-01-13 09:25:04 +0100

    tp3

--- polygone.h
+++ polygone.h
@@ -1,8 +1,18 @@
 /*** POLYGONE H ***/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template<typename T>
 
 class Polygone {
+
     private:
-        
+
     public:
+        Polygone();
+        Polygone(Vector<Point2D<T>< listeSommets);
+        Polygone(Polygone<T> poly);
+        
 
 };
