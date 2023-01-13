4fd19253dd2106cfe2f6b14e2726d397d2b431dc
Clément Guidon <101247576+clementguid@users.noreply.github.com>
2023-01-13 08:13:03 +0000

    Modify point2D files

---
+++ point2d.h
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class point2D{
+private:
+  int x;
+  int y;
+
+public:
+
+//constructeurs
+  point2D();
+  point2D(Tx, Ty);
+  Point2D (Point2D<T> p);
+
+//accesseurs en lecture
+  int getX() {return x;}
+  int getY() {return y;}
+
+//accesseurs en écriture
+  void setX(int Tx) {this->x = Tx};
+  void setY(int Ty) {this->y = Ty};
+  void translate (int Tx, int T);
+
+
+
+
+  
+  
+
+
+
+
+}
+
+
+  
+  
+
+
+
+
+}
