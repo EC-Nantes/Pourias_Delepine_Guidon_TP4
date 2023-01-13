4fbaf70799258219be523a9d8eca6ac966090c2a
RobinDELEPINE <robin.delepine1901@gmail.com>
2023-01-13 10:08:16 +0100

    modifications de parcelle.cpp
    creation des fonctions

--- parcelle.cpp
+++ parcelle.cpp
@@ -1,2 +1,40 @@
 #include "parcelle.h"
 
+Parcelle::Parcelle(int num, string prop, Polygone<int,float> forme){
+
+}
+
+Parcelle::Parcelle(Parcelle parc){
+
+}
+
+int Parcelle::getNumero(){
+
+}
+
+string Parcelle::getPropriétaire( ){
+
+}
+
+float Parcelle::getSurface( ){
+
+}
+
+Polygone<int,float> Parcelle::getForme( ){
+
+}
+string Parcelle::getType( ){
+
+}
+void Parcelle::setNumero(int n){
+
+}
+void Parcelle::setPropriétaire(string prop ){
+
+}
+void Parcelle::setForme(Polygone<int,float> forme){
+
+}
+void Parcelle::setType(string type){
+    
+}
